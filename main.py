3f168f54a571c51b069b959b4ee55f906676c881
ivwvh <68565784+ivwvh@users.noreply.github.com>
2023-11-16 17:03:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-
+import pynput
 """
 Необходимо реализовать игру «Ловкий муравьед».
 Главный герой – голодный, но очень ловкий муравьед бегает по двумерному полю от
@@ -26,30 +25,58 @@
 """
 
 
-ROWS = 2
-COLUMNS = 2
-
-
-class Field:
-    def __init__(self) -> None:
-        self.field = [
-            [FieldCell(x, y) for x in range(COLUMNS)] for y in range(ROWS)
-        ]
-
-    def draw_field(self) -> None:
-        for i in self.field:
-            print(*i)
+ROWS = 5
+COLUMNS = 5
 
 
 class FieldCell:
-    def __init__(self, x:int, y:int, image='.') -> None:
+    def __init__(self, x: int, y: int, image='.') -> None:
         self.image = image
         self.y = y
         self.x = x
+        self.content = None 
+
+    def __str__(self):
+        return self.image
+
+
+class Player:
+    def __init__(self, x=0, y=0, image='@') -> None:
+        self.x = x
+        self.y = y
+        self.image = image
 
     def __str__(self) -> str:
         return self.image
 
 
-test = Field()
-test.draw_field()
+class Field:
+    def __init__(self,
+                 columns: int,
+                 rows: int,
+                 player: Player) -> None:
+        self.columns = columns
+        self.rows = rows
+        self.player = player
+
+    def generate_field(self) -> None:
+        self.cells = [
+            [FieldCell(x, y) for x in range(self.columns)] for y in range(self.rows)
+        ]
+        self.cells[self.player.y ][self.player.x ].content = self.player 
+
+    def draw_field(self) -> None:
+        for row in self.cells:
+            for cell in row:
+                if cell.content:
+                    print(cell.content.image, end='')
+                else:
+                    print('.', end='')
+            print('\n')
+
+
+player = Player()
+field = Field(COLUMNS, ROWS, player=player)
+player.x, player.y = field.columns // 2, field.rows // 2
+field.generate_field()
+field.draw_field()
